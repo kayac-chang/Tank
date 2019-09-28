b3aedb40591d6b2d9696c142078fcd21b487681e
KayacChang <egg734631@gmail.com>
2019-09-28 15:04:14 +0800

    First Commit

--- project/src/game/interface/data/fonts.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    google: {
-        families: ['Candal', 'Basic'],
-    },
-};
-
-export const fonts = [
-    {name: 'font', url: '', metadata: config},
-];
--- project/src/service/utils/URL.js
+++
@@ -1,9 +0,0 @@
-const url = new URL(location);
-
-export function getSearchParam(key) {
-    return url.searchParams.get(key);
-}
-
-export function removeSearchParam(key) {
-    return url.searchParams.delete(key);
-}
